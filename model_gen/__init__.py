de8da812b61e99a4e83dc99c735cae9abd8e6abf
Miroslav Paris <miroslav.paris@deutsche-boerse.com>
2024-02-13 16:07:35 +0100

    remove model_gen

--- model_gen/__init__.py
+++
@@ -1,7 +0,0 @@
-__version__ = '2.0.0'
-
-from model_gen.config import settings
-
-get_current_env = settings.current_env
-
-__all__ = ["get_current_env"]
--- setup.py
+++ setup.py
@@ -1,37 +1,55 @@
+# coding: utf-8
+"""
+    Cloud Prisma Margin Estimator API
+
+    Cloud Prisma Margin Estimator (CPME) calculates margin for an uploaded portfolio according to Eurex PRISMA methodology.
+    The application is available to both members and non-members of Eurex Clearing.
+
+    OpenAPI spec version: 3.0
+
+    To install the library, run the following
+    $ pip install .
+
+    prerequisite: setuptools
+    http://pypi.python.org/pypi/setuptools
+"""
 from os import path
-from setuptools import setup
+from setuptools import setup, find_packages
 
-name = "cpme-api-client-gen"
+NAME = "cpme-api-client"
+VERSION = "1.0.0"
 here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
-    readme = f.read()
 
 # get the dependencies and installs
 with open("requirements.txt", encoding="utf-8") as f:
     requires = [x.strip() for x in f if x.strip()]
 
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    readme = f.read()
+
+
 setup(
-    name=name,
-    version='2.0.0',
-    description="Easy access to CPME 2.0 api with data model generator.",
+    name=NAME,
+    version=VERSION,
+    description="Cloud Prisma Margin Estimator API",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.deutsche-boerse.de/dev/DAVe-MarginEstimator-PythonAPIClient",
-    python_requires=">=3.8, <3.11",
-    install_requires=requires,
     author="Miroslav Paris",
-    entry_points={
-        "console_scripts":
-            ["comet = comet.cli:main", "model_gen = model_gen.cli:main"]
-    },
-    setup_requires=['wheel'],
-    keywords="dave, margining, risk, estimator, marginestimator, test, risk-it",
+    author_email="miroslav.paris@deutsche-boerse.com",
+    install_requires=requires,
+    url="https://github.deutsche-boerse.de/dev/DAVe-MarginEstimator-PythonAPIClient",
+    keywords="margining, risk, estimator, marginestimator, risk-it",
+    python_requires=">=3.8, <3.11",
+    packages=find_packages(),
+    include_package_data=True,
+    setup_requires=['setuptools>=59.0"'],
     classifiers=[
         "Development Status :: 1 - Beta",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
     ],
 )
