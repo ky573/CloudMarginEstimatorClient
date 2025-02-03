d62d2b445dea8257c9826208d7b9f8b11ccd52d3
Daniel Lopata <me@me.de>
2025-02-03 11:45:37 +0100

    removing print statements

--- margin_estimator_tool/src/margin_estimator_tool/series/series_request_handler.py
+++ margin_estimator_tool/src/margin_estimator_tool/series/series_request_handler.py
@@ -78,7 +78,6 @@ class SeriesRequestHandler(RequestHandler):
                                            live_timestamp=self.timestamp,
                                            live=self.version)
             self._check_for_error_in_response(response)
-            print(json.dumps(response, indent=4))
             response = response.get("list_series", [])
             return response
         except Exception as e:
